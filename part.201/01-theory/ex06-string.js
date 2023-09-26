6b84e2d38d1fcf4c13f4fa628335f2888b586207
Sergei Kamianetskii <i@ksergey.ru>
2023-09-26 12:21:14 +0300

    Пример строк js

---
+++ part.201/01-theory/ex06-string.js
@@ -0,0 +1,19 @@
+// I'm a programmer
+// I'm + a programmer
+
+// console.log('\\');
+// console.log("I'm ");
+// console.log(`a programmer`);
+// console.log(`a programmer`);
+// console.log(`a programmer`);
+// console.log(`a programmer`);
+
+console.log("I'm + a programmer\n  a programmer\n  a programmer");
+console.log("\\");
+console.log('I\'m a programmer');
+console.log("I'm a programmer");
+console.log('I"m a programmer');
+console.log('====');
+console.log('\t+');
+console.log('====');
+console.log('\r\n');
