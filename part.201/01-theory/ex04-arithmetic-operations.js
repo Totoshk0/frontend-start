510280829f1787192077d000b604f4b832c310c1
Sergei Kamianetskii <i@ksergey.ru>
2023-09-26 11:52:35 +0300

    Пример арифметических операций

---
+++ part.201/01-theory/ex04-arithmetic-operations.js
@@ -0,0 +1,39 @@
+// * — multiplication
+console.log(10 * 2); // 20
+
+// / — division
+console.log(10 / 2); // 5
+console.log(10 / 4); // 5
+
+// - — subtraction
+
+console.log(10 - 2); // 8
+
+// % — modulo
+console.log(10 % 2); // 0
+console.log(7 % 3); // 1
+console.log(15 % 4); // 3
+
+// ** — exponentiation
+console.log(10 ** 2); // 100
+console.log((+10) ** 2); // 100
+
+// - — inverting
+console.log(-10); // -10
+
+// operator precedence
+console.log(10 ** (4 - 1));
+console.log(10 ** 4 - 2);
+console.log(2 + 2 * 2);
+console.log((2 + -2) * 2);
+console.log(2 - (-2));
+
+// a = b · q + r,
+// где a — делимое,
+// b — делитель,
+// q — неполное частное,
+// r — остаток. 0 ⩽ r < | b |.
+
+console.log(-20 % 7);
+// 4 - 5
+// -10
