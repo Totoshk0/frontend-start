38b68ce76b31e888335b1565207e564b5f96977b
Sergei Kamianetskii <i@ksergey.ru>
2023-09-26 12:21:44 +0300

    Пример переменных и констант js

---
+++ part.201/01-theory/ex07-variables-constants.js
@@ -0,0 +1,15 @@
+// let идентификатор_переменной;
+// идентификатор_переменной = начальное_значение;
+
+// Инициализация != объявление
+// = - это не "равенство", а оператор присваивания
+let age = 30;
+console.log(age);
+
+age = 32;
+console.log(age);
+
+const firstName = 'John';
+// firstName = 'Robert'; //
+const lastName = 'Doe'
+console.log(firstName + ' ' + lastName + ', ' + age + ' y.o.');
