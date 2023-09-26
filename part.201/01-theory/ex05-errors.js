ca033e49f9fd4ada85044849d9d63d0001886ee8
Sergei Kamianetskii <i@ksergey.ru>
2023-09-26 11:59:24 +0300

    Пример ошибок js

---
+++ part.201/01-theory/ex05-errors.js
@@ -0,0 +1,13 @@
+// Три основных вида вида ошибок
+// 1. Ошибки Синтаксические
+// 2. Ошибки Логические
+// 3. Ошибки Выполнения
+
+console.log('hello bug');
+console.log("hello bug");
+console.log(`hello bug`);
+console.log(2 + 2 * -2);
+
+// console.log('hello bug);
+// console.log('hello bug`);
+// console.log(«hello bug»);
