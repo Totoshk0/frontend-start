970a92807da83a253f56c2800d2e21683f7aa31b
Sergei Kamianetskii <i@ksergey.ru>
2023-10-05 21:36:08 +0300

    Примеры логических операторов

---
+++ part.205/ex03.js
@@ -0,0 +1,34 @@
+// Логические операторы
+
+const logicFalse = false;
+const logicTrue = true;
+
+const check1 = 8 > 11;
+console.log(`check1: ${check1}`);
+
+const check2 = 12 >= 3;
+console.log(`check2: ${check2}`);
+
+const check3 = 3 < 4;
+console.log(`check3: ${check3}`);
+
+const check4 = 5 <= 5;
+console.log(`check4: ${check4}`);
+
+const check5 = 1 === 1;
+console.log(`check5: ${check5}`);
+
+const check6 = 8 !== 8;
+console.log(`check6: ${check6}`);
+
+
+const check7 = 'text 1' === 'text 1';
+console.log(`check7: ${check7}`);
+
+const check8 = 'text 1' !== 'text 2';
+console.log(`check8: ${check8}`);
+
+const check9 = 'text 1' === 'text 2';
+console.log(`check9: ${check9}`);
+
+// Есть более сложные сценарии, которые будем изучать позже
