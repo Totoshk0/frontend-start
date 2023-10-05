cb7a1ed9f1f0fe342696ace7d478cb4041fead7b
Sergei Kamianetskii <i@ksergey.ru>
2023-10-05 22:14:25 +0300

    Примеры логических операций на js

---
+++ part.205/ex06.js
@@ -0,0 +1,25 @@
+// Логические операции
+
+// И
+// const and = (x, y) {
+//   return x && y;
+// }
+const and = (x, y) => x && y;
+
+// ИЛИ
+// const or = (x, y) {
+//   return x || y;
+// }
+const or = (x, y) => x || y;
+
+// НЕ
+// const not = (x) {
+//   return !x;
+// }
+const not = (x) => !x;
+
+// ИСКЛЮЧАЮЩЕЕ ИЛИ
+// const xor = (x, y) {
+//   return x ^ y;
+// }
+const xor = (x, y) => x ^ y;
