ec9e8edf7e11bd2a75cec5fc11c16ca63e5e961b
Sergei Kamianetskii <i@ksergey.ru>
2023-10-06 10:35:47 +0300

    Необязательные  параметры функции. дополнительный пример

--- part.205/ex01.js
+++ part.205/ex01.js
@@ -8,6 +8,17 @@ const printText2 = (text = '') => text;
 //   return text;
 // }
 
+// const average = (x, y = 0, z = 1) => {
+//   return (x + y + z) / 3;
+// }
+const average = (x, y = 0, z = 1) => (x + y + z) / 3;
+
+let a = average(4, 4);
+console.log(`a = ${a}`);
+a = average(7);
+console.log(`a = ${a}`);
+
+
 let result = printText1();
 console.log(typeof result); // undefined -> undefined
 
