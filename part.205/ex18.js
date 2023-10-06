b5c6672ebb4b4567ee26ec492e82d5c2516ee81a
Sergei Kamianetskii <i@ksergey.ru>
2023-10-06 11:19:02 +0300

    Добавлен пример сравнения чисел с учетом их равенства

---
+++ part.205/ex18.js
@@ -0,0 +1,12 @@
+const value1 = 100;
+const value2 = 100;
+
+if (value1 === value2) {
+  console.log('Числа равны');
+} else {
+  if (value1 < value2) {
+    console.log(value2);
+  } else {
+    console.log(value1);
+  }
+}
